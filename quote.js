aaf489b6598dcf960f72e613faffecd329057424
cabs-com <arjaycaburao3@gmail.com>
2025-12-19 21:32:41 +0800

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,15 +1,10 @@
 const quoteEl = document.getElementById('quote');
 
     async function loadQuote() {
-      try {
         const res = await fetch('https://zenquotes.io/api/random');
         const data = await res.json();
-        const quote = data[0]; // ZenQuotes returns an array
+        const quote = data[0];
         quoteEl.textContent = `“${quote.q}” — ${quote.a}`;
-      } catch (err) {
-        quoteEl.textContent = "Oops! Could not load a quote.";
-        console.error(err);
-      }
     }
 
     loadQuote();
